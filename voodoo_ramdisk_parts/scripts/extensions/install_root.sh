c0599563cd1104e9285db87d1f6a3cde416a70cf
SIMOND François <francois@lienweb.fr>
2010-10-20 16:54:05 +0200

    improved comment 2nd

--- voodoo_ramdisk_parts/scripts/extensions/install_root.sh
+++ voodoo_ramdisk_parts/scripts/extensions/install_root.sh
@@ -8,17 +8,24 @@ extension_install_su() {
 	cat /voodoo/root/sbin/su > $su_dest
 	# make sure it's owned by root
 	chown root.shell $su_dest
+	# sets the suid permission
 	chmod 06755 $su_dest
 	log "secure su binary installed"
 }
 
 if test -u $su_dest ; then
+
+	# okay, the su binary exist and is already suid
 	if test /voodoo/root/sbin/su -nt $su_dest; then
+
+		# but it's older than ours ! let's updated it
 		extension_install_su
 	else
+		# ours is the same or older, don't touch it
 		log "secure su binary already installed"
 	fi	
 else
+	# not here or not setup properly, let's install su
 	extension_install_su
 fi
 	
