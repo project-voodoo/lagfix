167c00592e99ec5db2c4b86ea7a76220ffc92681
SIMOND François <francois@lienweb.fr>
2010-11-01 02:38:10 +0100

    some comments in init_runner.sh

--- voodoo_ramdisk_parts/scripts/init_runner.sh
+++ voodoo_ramdisk_parts/scripts/init_runner.sh
@@ -12,6 +12,8 @@ mount -t vfat -o utf8 /dev/block/mmcblk0p1 /voodoo/tmp/sdcard
 #/voodoo/scripts/init.sh > /voodoo/logs/init.log 2>&1
 /voodoo/scripts/init.sh >> /voodoo/tmp/sdcard/init.log 2>&1
 
+# umount the sdcard before running Samsung's init
 umount /voodoo/tmp/sdcard
 
+# finally run Samsung's android init binary
 exec /init_samsung
