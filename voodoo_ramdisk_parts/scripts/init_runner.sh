79fe0ce720f0e246acd2b736267a11a995ff1ea2
SIMOND François <francois@lienweb.fr>
2010-11-21 04:44:28 +0100

    move boot fails logs to sdcard/Voodoo/logs

--- voodoo_ramdisk_parts/scripts/init_runner.sh
+++ voodoo_ramdisk_parts/scripts/init_runner.sh
@@ -2,19 +2,21 @@
 # logger for Voodoo init script
 
 PATH=/bin:/sbin:/voodoo/scripts
+sdcard='/voodoo/tmp/sdcard'
 
 # create used devices nodes
 create_devices.sh
 
 # mount the sdcard for all Galaxy S || or Fascinate
-mount -t vfat -o utf8 /dev/block/mmcblk0p1 /voodoo/tmp/sdcard || \
-mount -t vfat -o utf8 /dev/block/mmcblk1p1 /voodoo/tmp/sdcard
+mount -t vfat -o utf8 /dev/block/mmcblk0p1 $sdcard || \
+mount -t vfat -o utf8 /dev/block/mmcblk1p1 $sdcard
 
-mv /voodoo/tmp/sdcard/init.log /voodoo/tmp/sdcard/voodoo-init-failed-boot-log-`date '+%Y-%m-%d_%H-%M-%S'`.txt
-/voodoo/scripts/init.sh 2>&1 | tee /voodoo/tmp/sdcard/init.log > /voodoo/logs/init.log
+mkdir -p /voodoo/tmp/sdcard/Voodoo/logs
+mv $sdcard/init.log $sdcard/Voodoo/logs/init-failed-boot-log-`date '+%Y-%m-%d_%H-%M-%S'`.txt
+/voodoo/scripts/init.sh 2>&1 | tee $sdcard/init.log > /voodoo/logs/init.log
 
 # umount the sdcard before running Samsung's init
-umount /voodoo/tmp/sdcard
+umount $sdcard
 
 # finally run Samsung's android init binary
 exec /init_samsung
