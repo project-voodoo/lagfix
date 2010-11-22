a6b0b774dd016e006e6794214689e385f6702165
SIMOND François <francois@lienweb.fr>
2010-11-22 06:08:43 +0100

    auto detect quickly Fascinate for sdcard mount

--- voodoo_ramdisk_parts/scripts/init_runner.sh
+++ voodoo_ramdisk_parts/scripts/init_runner.sh
@@ -7,9 +7,19 @@ PATH=/bin:/sbin:/voodoo/scripts
 # create used devices nodes
 create_devices.sh
 
-# mount the sdcard for all Galaxy S || or Fascinate
-mount -t vfat -o utf8 /dev/block/mmcblk0p1 $sdcard || \
-mount -t vfat -o utf8 /dev/block/mmcblk1p1 $sdcard
+# proc and sys are  used
+mount -t proc proc /proc
+mount -t sysfs sys /sys
+
+# mount the sdcard for Galaxy S and Fascinate
+# detect Fascinate
+if test "`cat /sys/block/mmcblk0/size`" = 3907584; then
+	# we are on fascinate,
+	mount -t vfat -o utf8 /dev/block/mmcblk1p1 $sdcard
+else
+	# every other Galaxy S
+	mount -t vfat -o utf8 /dev/block/mmcblk0p1 $sdcard
+fi
 
 # save the logs written during unfinished boots
 mv $log_dir $sdcard/Voodoo/logs/boot-`date '+%Y-%m-%d_%H-%M-%S'`-error
