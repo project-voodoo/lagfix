b512da11a6746119dccf841a061ade9d311d7910
SIMOND François <francois@lienweb.fr>
2010-11-24 16:08:41 +0100

    improved mount_wrapper so CWM works with default vibrant boardconfig

--- voodoo_ramdisk_parts/scripts/mount_wrapper.sh
+++ voodoo_ramdisk_parts/scripts/mount_wrapper.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-if test "$2" = "auto"; then
+echo "mount command: $0 $*" > /voodoo/logs/mount_wrapper_log.txt
+if test "$2" = 'auto' || test "$2" = 'rfs'; then
 	/bin/mount -t ext4 -o noatime,nodev,barrier=0,data=writeback $4 $5 || /bin/mount $*
 else
 	/bin/mount $*
