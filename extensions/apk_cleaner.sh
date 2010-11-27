8867f6d13ca4d21df24d9462c2320cbc7f354f0a
SIMOND François <francois@lienweb.fr>
2010-11-27 23:06:59 +0100

    ensure zip binary is available in apk_cleaner extension

--- extensions/apk_cleaner.sh
+++ extensions/apk_cleaner.sh
@@ -2,13 +2,21 @@
 
 zip='/voodoo/extensions/apk_cleaner/zip'
 
-# clean apk's inside!
-apk='/system/app/TouchWiz30Launcher.apk'
-if test -f $apk; then
-	if test `du -s $apk | cut -d/ -f1` -gt 1500 ; then
-		$zip -d $apk res/*1024x600*
-		log "TouchWiz 3.0 Launcher cleaned up: apk now optimized"
-	else
-		log "TouchWiz 3.0 Launcher already optimized"
+# ensure the zip binary is available
+if test -x $zip; then
+
+	# clean apk's inside!
+	apk='/system/app/TouchWiz30Launcher.apk'
+	if test -f $apk; then
+		if test `du -s $apk | cut -d/ -f1` -gt 1500 ; then
+			$zip -d $apk res/*1024x600*
+			log "TouchWiz 3.0 Launcher cleaned up: apk now optimized"
+		else
+			log "TouchWiz 3.0 Launcher already optimized"
+		fi
 	fi
+
+else
+	log "ERROR: apk_cleaner cannot do its job. zip binary missing"
+	log "please contact your Voodoo lagfix kernel vendor to notify this error"
 fi
