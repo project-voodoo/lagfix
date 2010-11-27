5bb3e4c51dce8fcaac5c3e8beb68094a9ddf5587
SIMOND François <francois@lienweb.fr>
2010-11-27 08:37:43 +0100

    sstrip on the binary: 150kB -> 130kB

--- zip/scripts/build.sh
+++ zip/scripts/build.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 cd zip30
 ln -s ../../addons/Makefile-voodoo unix/
 make -f unix/Makefile-voodoo clean
 make -f unix/Makefile-voodoo -j8 generic
+
+echo
+ls -lh zip
+echo -e "\nStriping the binary with sstrip"
+../../stages_builder/buildroot-2010.08/output/staging/usr/arm-unknown-linux-uclibcgnueabi/bin/sstrip zip
+echo
+ls -lh zip
+
