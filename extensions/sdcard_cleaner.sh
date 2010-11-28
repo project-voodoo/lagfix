5e227b5675c4dc9cbd4e779905d1742cf2314b62
SIMOND François <francois@lienweb.fr>
2010-11-28 23:27:09 +0100

    log filenames deleted by the sdcard cleaner extension

--- extensions/sdcard_cleaner.sh
+++ extensions/sdcard_cleaner.sh
@@ -1,9 +1,9 @@
 # Voodoo lagfix extension
 
 # clean sdcard LOST.DIR that tend to grow despite the clean mount / umount
-rm -rf /sdcard/LOST.DIR
+rm -rvf /sdcard/LOST.DIR
 
 # these files are created on Froyo but as annoying as useless
-rm -rf /sdcard/DiskCacheIndex*.tmp
+rm -rvf /sdcard/DiskCacheIndex*.tmp
 
 log "sdcard cleaned up"
