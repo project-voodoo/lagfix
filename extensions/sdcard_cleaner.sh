c360110de61889182e8c5ebfe970a0dbb2b39b05
SIMOND François <francois@lienweb.fr>
2010-11-29 04:47:56 +0100

    don't delete the LOST.DIR directory, only what's inside of it

--- extensions/sdcard_cleaner.sh
+++ extensions/sdcard_cleaner.sh
@@ -1,7 +1,7 @@
 # Voodoo lagfix extension
 
 # clean sdcard LOST.DIR that tend to grow despite the clean mount / umount
-rm -rvf /sdcard/LOST.DIR
+rm -rvf /sdcard/LOST.DIR/*
 
 # these files are created on Froyo but as annoying as useless
 rm -rvf /sdcard/DiskCacheIndex*.tmp
