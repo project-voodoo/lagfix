e9dc810ed3e6695c767a231a13194d0746b1159e
SIMOND François <francois@lienweb.fr>
2010-12-04 07:29:15 +0100

    don't keep data and dbdata unneccessary mounted in CWM

--- voodoo_ramdisk_parts/scripts/cwm_start.sh
+++ voodoo_ramdisk_parts/scripts/cwm_start.sh
@@ -174,6 +174,8 @@ mkdir -p /etc
 mkdir -p /datadata
 chmod 4777 /sbin/su
 umount /efs
+umount /dbdata
+umount /data
 
 # succeed to mount the sdcard by default even with broken fstab
 mount -t vfat -o rw,nosuid,nodev,noexec,uid=1000,gid=1015,fmask=0002,dmask=0002,allow_utime=0020,iocharset=iso8859-1,shortname=mixed,utf8,errors=remount-ro "`cat /voodoo/run/sdcard_device`" /sdcard
