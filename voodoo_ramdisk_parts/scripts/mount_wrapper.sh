e197a59350b8944d8274f65b5a451361b93a2831
SIMOND François <francois@lienweb.fr>
2010-12-05 13:23:32 +0100

    remove nodev mount option, not useful here

--- voodoo_ramdisk_parts/scripts/mount_wrapper.sh
+++ voodoo_ramdisk_parts/scripts/mount_wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "mount command: $0 $*" > /voodoo/logs/mount_wrapper_log.txt
 if test "$2" = 'auto' || test "$2" = 'rfs'; then
-	/bin/mount -t ext4 -o noatime,nodev,barrier=0,data=ordered $4 $5 || /bin/mount $*
+	/bin/mount -t ext4 -o noatime,barrier=0,data=ordered $4 $5 || /bin/mount $*
 else
 	/bin/mount $*
 fi
